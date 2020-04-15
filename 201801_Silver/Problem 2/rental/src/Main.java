3e44048523b536d59ca9873fb8e20bfde142c4a7
Siyuan Chen <daniel@apextone.com>
2020-04-15 14:23:56 -0400

    Added rental

---
+++ 201801_Silver/Problem 2/rental/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("rental.in"));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        br.close();
+
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("rental.out")));
+
+        pw.close();
+
+    }
+
+
+}
