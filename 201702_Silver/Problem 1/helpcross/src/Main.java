def552f59ee4d3c0e1f48e2821a44454b7396b75
Siyuan Chen <daniel@apextone.com>
2020-04-16 13:14:33 -0400

    Added helpcross

---
+++ 201702_Silver/Problem 1/helpcross/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("helpcross.in"));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        br.close();
+
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("helpcross.out")));
+
+        pw.println();
+
+    }
+
+
+}
