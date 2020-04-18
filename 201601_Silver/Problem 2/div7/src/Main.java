0f890b90be320d5950dd3e7c3a313371f0ccc353
Siyuan Chen <daniel@apextone.com>
2020-04-17 23:20:36 -0400

    Added div7

---
+++ 201601_Silver/Problem 2/div7/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("div7.in"));
+
+        br.close();
+
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("div7.out")));
+
+        pw.close();
+
+    }
+
+}
