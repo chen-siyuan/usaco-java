7d70fb38105b964663f309b5daca2f59b68ec286
Siyuan Chen <daniel@apextone.com>
2020-04-21 16:20:44 -0400

    Added pails

---
+++ 201602_Silver/Problem 3/pails/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("pails.in"));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        br.close();
+
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("pails.out")));
+
+        pw.close();
+
+    }
+
+}
