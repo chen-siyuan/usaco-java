8aa9c10faf9725591c729f6745d15c841be1e7a4
Siyuan Chen <daniel@apextone.com>
2020-04-21 16:17:53 -0400

    Added cbarn

---
+++ 201602_Silver/Problem 1/cbarn/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("cbarn.in"));
+
+        br.close();
+
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("cbarn.out")));
+
+        pw.close();
+
+    }
+
+}
