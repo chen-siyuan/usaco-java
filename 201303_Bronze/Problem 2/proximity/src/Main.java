4734636c2e1ed4d4dd2f9ce81eb7c53b5c2edd69
Siyuan Chen <daniel@apextone.com>
2020-04-24 17:36:10 -0400

    Solved proximity

--- 201303_Bronze/Problem 2/proximity/src/Main.java
+++ 201303_Bronze/Problem 2/proximity/src/Main.java
@@ -1,7 +1,42 @@
 import java.io.*;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class Main {
+
+    public static int numCows;
+    public static int maxDistance;
+    public static TreeMap<Integer, TreeSet<Integer>> indexLists;
+
+    public static int maxIndex() {
+
+        while(indexLists.size() > 0) {
+
+            int key = indexLists.lastKey();
+            TreeSet<Integer> indices = indexLists.get(key);
+            indexLists.remove(key);
+
+            if(indices.size() == 1) continue;
+
+            int previousIndex = indices.first();
+            indices.remove(previousIndex);
+
+            while(indices.size() > 0) {
+
+                int thisIndex = indices.first();
+                indices.remove(thisIndex);
+
+                if(thisIndex - previousIndex <= maxDistance) return key;
+
+                previousIndex = thisIndex;
+
+            }
+
+        }
+
+        return -1;
+    }
 
     public static void main(String[] args) throws IOException {
 
@@ -9,9 +44,24 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        numCows = Integer.parseInt(st.nextToken());
+        maxDistance = Integer.parseInt(st.nextToken());
+        indexLists = new TreeMap<>();
+
+        for(int i=0; i < numCows; i++) {
+
+            int temp = Integer.parseInt(br.readLine());
+
+            if(!indexLists.containsKey(temp)) indexLists.put(temp, new TreeSet<>());
+            indexLists.get(temp).add(i);
+
+        }
+
         br.close();
 
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("proximity.out")));
+
+        pw.println(maxIndex());
 
         pw.close();
 
