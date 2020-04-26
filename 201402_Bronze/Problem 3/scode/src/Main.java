cfbc81226d2927c3cfa1da14f10cc6736f586229
Siyuan Chen <daniel@apextone.com>
2020-04-25 20:01:03 -0400

    Added scode

---
+++ 201402_Bronze/Problem 3/scode/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("scode.in"));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        br.close();
+
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("scode.out")));
+
+        pw.close();
+
+    }
+
+}
