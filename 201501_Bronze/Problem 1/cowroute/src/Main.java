91ef8e61e1159ae943f9dec9e6c0737c7809826c
Siyuan Chen <daniel@apextone.com>
2020-05-02 18:22:45 -0400

    Solved cowroute

--- 201501_Bronze/Problem 1/cowroute/src/Main.java
+++ 201501_Bronze/Problem 1/cowroute/src/Main.java
@@ -3,15 +3,51 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    public static int origin;
+    public static int destination;
+    public static boolean departed;
+    public static int numRoutes;
+    public static int min;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new FileReader("cowroute.in"));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        origin = Integer.parseInt(st.nextToken());
+        destination = Integer.parseInt(st.nextToken());
+        numRoutes = Integer.parseInt(st.nextToken());
+
+        min = Integer.MAX_VALUE;
+
+        for(int i=0; i < numRoutes; i++) {
+
+            departed = false;
+
+            st = new StringTokenizer(br.readLine());
+
+            int cost = Integer.parseInt(st.nextToken());
+            int numCities = Integer.parseInt(st.nextToken());
+
+            st = new StringTokenizer(br.readLine());
+
+            for(int j=0; j < numCities; j++) {
+
+                int city = Integer.parseInt(st.nextToken());
+
+                if(city == origin) departed = true;
+                if(departed && city == destination) min = Math.min(min, cost);
+
+            }
+
+        }
+
         br.close();
 
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("cowroute.out")));
+
+        pw.println(min == Integer.MAX_VALUE ? -1 : min);
 
         pw.close();
 
