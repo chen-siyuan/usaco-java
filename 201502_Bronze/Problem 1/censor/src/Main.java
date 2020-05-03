45081045eced84fd7c4f6ab126aed9dac4266fc6
Siyuan Chen <daniel@apextone.com>
2020-05-02 23:26:58 -0400

    Solved censor

--- 201502_Bronze/Problem 1/censor/src/Main.java
+++ 201502_Bronze/Problem 1/censor/src/Main.java
@@ -2,13 +2,47 @@ import java.io.*;
 
 public class Main {
 
+    public static String raw;
+    public static String trigger;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new FileReader("censor.in"));
 
-        br.close();
+        raw = br.readLine();
+        trigger = br.readLine();
+
+        StringBuilder result = new StringBuilder();
+        StringBuilder buffer = new StringBuilder();
+
+        for(int i=0; i < raw.length(); i++) {
+
+            buffer.append(raw.charAt(i));
+
+            if(buffer.length() < trigger.length()) continue;
+
+            if(trigger.equals(buffer.toString())) {
+
+                buffer = new StringBuilder();
+
+                int temp = result.length();
+
+                for(int j=0; j < Math.min(temp, trigger.length() - 1); j++) {
+                    buffer.insert(0, result.charAt(result.length() - 1));
+                    result.deleteCharAt(result.length() - 1);
+                }
+
+            } else {
+                result.append(buffer.charAt(0));
+                buffer.deleteCharAt(0);
+            }
+
+        }
 
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("censor.out")));
+
+        pw.print(result.toString());
+        pw.println(buffer.toString());
 
         pw.close();
 
