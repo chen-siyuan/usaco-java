52699f6ac0ee753022fec74777344b48f2a6d384
Siyuan Chen <daniel@apextone.com>
2020-05-02 23:40:50 -0400

    Solved cow

--- 201502_Bronze/Problem 2/cow/src/Main.java
+++ 201502_Bronze/Problem 2/cow/src/Main.java
@@ -6,9 +6,31 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new FileReader("cow.in"));
 
+        int length = Integer.parseInt(br.readLine());
+
+        char[] letters = br.readLine().toCharArray();
+
         br.close();
 
+        int cCount = 0;
+        int wCount = 0;
+
+        for(int i=0; i < length; i++) if(letters[i] == 'W') wCount++;
+
+        long total = 0;
+
+        for(int i=0; i < length; i++) switch(letters[i]) {
+            case 'C':
+                cCount++; break;
+            case 'O':
+                total += cCount * wCount; break;
+            case 'W':
+                wCount--; break;
+        }
+
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("cow.out")));
+
+        pw.println(total);
 
         pw.close();
 
