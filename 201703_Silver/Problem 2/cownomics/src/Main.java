036cd67ab4c32cb7a8d1b0a27bbb00c4e7eab223
Siyuan Chen <daniel@apextone.com>
2020-05-04 22:14:18 -0400

    Added cownomics

---
+++ 201703_Silver/Problem 2/cownomics/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("cownomics.in"));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        br.close();
+
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("cownomics.out")));
+
+        pw.close();
+
+    }
+
+}
