9abe7b8396476f6b6c2e49b8c91ff525183353ee
Siyuan Chen <daniel@apextone.com>
2020-05-04 22:17:24 -0400

    Added where

---
+++ 201703_Silver/Problem 3/where/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("where.in"));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        br.close();
+
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("where.out")));
+
+        pw.close();
+
+    }
+
+}
