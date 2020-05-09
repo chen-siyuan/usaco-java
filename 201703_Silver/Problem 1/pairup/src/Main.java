2e3645654d0a7f6360814647827a748538285075
Siyuan Chen <daniel@apextone.com>
2020-05-08 20:02:33 -0400

    Solved pairup

--- 201703_Silver/Problem 1/pairup/src/Main.java
+++ 201703_Silver/Problem 1/pairup/src/Main.java
@@ -1,7 +1,48 @@
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    public static int numEntries;
+    public static Entry[] entries;
+    public static int sum;
+    public static int headPointer;
+    public static int tailPointer;
+    public static int headAmount;
+    public static int tailAmount;
+    public static int max;
+
+    public static void search() {
+
+        int increase = Math.min(entries[headPointer].getAmount() - headAmount,
+                entries[tailPointer - 1].getAmount() - tailAmount);
+
+        headAmount += increase;
+        tailAmount += increase;
+
+        max = Math.max(max, entries[headPointer].getValue() + entries[tailPointer - 1].getValue());
+
+        if(headAmount == entries[headPointer].getAmount()) {
+            headAmount = 0;
+            headPointer++;
+        }
+
+        if(tailAmount == entries[tailPointer - 1].getAmount()) {
+            tailAmount = 0;
+            tailPointer--;
+        }
+
+        if(headPointer == tailPointer - 1) {
+
+            if(headAmount + tailAmount != entries[headPointer].getAmount())
+                max = Math.max(max, entries[headPointer].getValue() + entries[tailPointer - 1].getValue());
+
+        } else {
+            search();
+        }
+
+    }
 
     public static void main(String[] args) throws IOException {
 
@@ -9,12 +50,67 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        numEntries = Integer.parseInt(st.nextToken());
+
+        entries = new Entry[numEntries];
+
+        for(int i=0; i < numEntries; i++) {
+
+            st = new StringTokenizer(br.readLine());
+            entries[i] = new Entry(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            sum += entries[i].getAmount();
+
+        }
+
         br.close();
+
+        Arrays.sort(entries);
+
+        headPointer = 0;
+        tailPointer = numEntries;
+        headAmount = 0;
+        tailAmount = 0;
+
+        max = Integer.MIN_VALUE;
+
+        search();
 
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("pairup.out")));
 
+        pw.println(max);
+
         pw.close();
 
+    }
+
+}
+
+class Entry implements Comparable<Entry> {
+
+    public final int amount;
+    public final int value;
+
+    public Entry(int amount, int value) {
+        this.amount = amount;
+        this.value = value;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public int compareTo(Entry entry) {
+        return value - entry.value;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %d", amount, value);
     }
 
 }
--- 201703_Silver/Problem 2/cownomics/src/Main.java
+++ 201703_Silver/Problem 2/cownomics/src/Main.java
@@ -1,7 +1,64 @@
 import java.io.*;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    public static int numCows;
+    public static int numDigits;
+    public static int[][] cows;
+
+    public static int transform(int a, int b, int c) {
+        return a + b * 4 + c * 16;
+    }
+
+    public static boolean check(int[] combination) {
+
+        HashSet<Integer> spotted = new HashSet<>();
+
+        for(int i=0; i < numCows; i++)
+            spotted.add(transform(cows[i][combination[0]], cows[i][combination[1]], cows[i][combination[2]]));
+
+        for(int i=numCows; i < numCows * 2; i++)
+            if(spotted.contains(transform(cows[i][combination[0]], cows[i][combination[1]], cows[i][combination[2]])))
+                return false;
+
+        return true;
+    }
+
+    public static int[][] generate() {
+
+        int[][] result = new int[numDigits * (numDigits - 1) * (numDigits - 2) / 6][3];
+        int pointer = 0;
+
+        for(int i=0; i < numDigits - 2; i++) for(int j=i + 1; j < numDigits - 1; j++) for(int k=j + 1; k < numDigits; k++) {
+
+                result[pointer][0] = i;
+                result[pointer][1] = j;
+                result[pointer][2] = k;
+
+                pointer++;
+
+        }
+
+        return result;
+    }
+
+    public static int convert(char gene) {
+
+        switch(gene) {
+            case 'A':
+                return 0;
+            case 'T':
+                return 1;
+            case 'C':
+                return 2;
+            case 'G':
+                return 3;
+        }
+
+        return -1;
+    }
 
     public static void main(String[] args) throws IOException {
 
@@ -9,9 +66,25 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        numCows = Integer.parseInt(st.nextToken());
+        numDigits = Integer.parseInt(st.nextToken());
+
+        cows = new int[numCows * 2][numDigits];
+
+        for(int i=0; i < numCows * 2; i++) {
+            String temp = br.readLine();
+            for(int j=0; j < numDigits; j++) cows[i][j] = convert(temp.charAt(j));
+        }
+
         br.close();
 
+        int count = 0;
+
+        for(int[] combination: generate()) if(check(combination)) count++;
+
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("cownomics.out")));
+
+        pw.println(count);
 
         pw.close();
 
