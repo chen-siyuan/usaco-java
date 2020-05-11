3b6aa183af7a49aaf8eabca3c1c1c31c18368e47
Siyuan Chen <daniel@apextone.com>
2020-05-11 11:24:47 -0400

    Solved homework

--- 201712_Silver/Problem 1/homework/src/Main.java
+++ 201712_Silver/Problem 1/homework/src/Main.java
@@ -1,7 +1,14 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    public static int numScores;
+    public static int[] scores;
+    public static int[] minScores;
+    public static int sum;
 
     public static void main(String[] args) throws IOException {
 
@@ -9,9 +16,45 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        numScores = Integer.parseInt(st.nextToken());
+
+        scores = new int[numScores];
+        sum = 0;
+
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i < numScores; i++) {
+            scores[i] = Integer.parseInt(st.nextToken());
+            sum += scores[i];
+        }
+
         br.close();
 
+        minScores = new int[numScores];
+
+        for(int i=numScores - 1; i >= 0; i--) minScores[i] = i == numScores - 1 ? scores[i] : Math.min(scores[i], minScores[i + 1]);
+
+        double maxScore = -1;
+        ArrayList<Integer> maxIndices = new ArrayList<>();
+
+        for(int i=0; i < numScores - 2; i++) {
+
+            sum -= scores[i];
+
+            double thisScore = (sum - minScores[i + 1]) / (numScores - i - 2.);
+
+            if(maxScore < thisScore) {
+                maxScore = thisScore;
+                maxIndices.clear();
+            }
+
+            if(maxScore == thisScore) maxIndices.add(i + 1);
+
+        }
+
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("homework.out")));
+
+        for(int index: maxIndices) pw.println(index);
 
         pw.close();
 
