44315f6b6835bf9f13a416543336a9271d143633
Siyuan Chen <daniel@apextone.com>
2020-05-12 17:16:08 -0400

    Added sort

---
+++ 201803_Silver/Problem 1/sort/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("sort.in"));
+
+        br.close();
+
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("sort.out")));
+
+        pw.close();
+
+    }
+
+}
