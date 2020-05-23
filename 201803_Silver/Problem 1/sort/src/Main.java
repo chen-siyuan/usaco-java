bc80f4bcb1dc6e471ba745f55c97ced262ea9a4e
Siyuan Chen <daniel@apextone.com>
2020-05-23 18:41:31 -0400

    Solved sort

--- 201803_Silver/Problem 1/sort/src/Main.java
+++ 201803_Silver/Problem 1/sort/src/Main.java
@@ -1,17 +1,71 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
+
+    public static int num;
+    public static Pair[] array;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new FileReader("sort.in"));
 
+        num = Integer.parseInt(br.readLine());
+
+        array = new Pair[num];
+
+        for(int i=0; i < num; i++) array[i] = new Pair(Integer.parseInt(br.readLine()), i);
+
         br.close();
+
+        Arrays.sort(array);
+
+        int max = 0;
+
+        for(int i=0; i < num; i++) max = Math.max(max, array[i].getIndex() - i);
 
         PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("sort.out")));
 
+        pw.println(max + 1);
+
         pw.close();
 
+    }
+
+}
+
+class Pair implements Comparable<Pair> {
+
+    public final int value;
+    public final int index;
+
+    public Pair(int value, int index) {
+        this.value = value;
+        this.index = index;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    @Override
+    public int compareTo(Pair pair) {
+
+        if(value == pair.value) {
+            return index - pair.index;
+        } else {
+            return value - pair.value;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %d", value, index);
     }
 
 }
