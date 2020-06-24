5ed4248e646da327bdc922c3732513889c25861e
Siyuan Chen <daniel@apextone.com>
2020-06-24 19:26:31 -0400

    Solved cowjump (finally)
    Note

--- 201903_Silver/Problem 2/cowjump/src/Test.java
+++ 201903_Silver/Problem 2/cowjump/src/Test.java
@@ -1,23 +1,7 @@
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.StringTokenizer;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author chensiyuan
- */
-public class Main {
+public class Test {
     
     public static void main(String[] args) throws IOException {
         
