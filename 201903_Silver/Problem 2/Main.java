43ca6e8e6f05ed47f6ea4e2769bc6a550fe72d94
Siyuan Chen <daniel@apextone.com>
2021-01-18 13:18:00 -0500

    Solved cowjump
    Sweeping line, segment intersection

--- 201903_Silver/Problem 2/Main.java
+++ 201903_Silver/Problem 2/Main.java
@@ -43,7 +43,6 @@ public class Main {
     public static void main(String[] args) {
 
         FastReader fr = new FastReader("cowjump.in");
-        // FastReader fr = new FastReader("test1.in");
 
         int n = fr.nextInt();
 
@@ -66,12 +65,6 @@ public class Main {
         for(int i=0; i < n * 2; i++) E[i] = i;
         Arrays.sort(E, Comparator.comparingInt(e -> X[e]));
 
-        // for(Integer e: E) System.out.printf("%d %d %d\n", e, X[e], Y[e]);
-
-        // for(int i=0; i < n - 1; i++) for(int j=i + 1; j < n; j++) if(check(X, Y, i, j)) {
-            // System.out.printf("%d %d\n", i, j);
-        // }
-
         int res0 = -1, res1 = -1;
         List<Integer> A = new ArrayList<>();
         for(Integer e: E) if(e % 2 == 0) {
@@ -91,24 +84,15 @@ public class Main {
             int tmp;
 
             if(index != 0 && check(X, Y, e / 2, (tmp = A.get(index - 1) / 2))) {
-                // System.out.printf("%d %d\n", e / 2, tmp);
                 res0 = Math.min(e / 2, tmp);
                 res1 = Math.max(e / 2, tmp);
                 break;
             }
             if(index != A.size() - 1 && check(X, Y, e / 2, (tmp = A.get(index + 1) / 2))) {
-                // System.out.printf("%d %d\n", e / 2, tmp);
                 res0 = Math.min(e / 2, tmp);
                 res1 = Math.max(e / 2, tmp);
                 break;
             }
-            // int tmp1;
-            // if(index != 0 && index != A.size() - 1 && check(X, Y, (tmp = A.get(index - 1) / 2), (tmp1 = A.get(index + 1) / 2))) {
-                // // System.out.printf("%d %d\n", e / 2, tmp);
-                // res0 = Math.min(tmp, tmp1);
-                // res1 = Math.max(tmp, tmp1);
-                // break;
-            // }
 
             A.remove(index);
 
@@ -116,15 +100,13 @@ public class Main {
 
         for(int i=0; i < n; i++) if(i != res0 && i != res1) if(check(X, Y, i, res1)) {
             try(PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("cowjump.out")))) {
-                // pw.println(res1 + 1);
-                System.out.println(res1);
+                pw.println(res1 + 1);
             } catch(Exception ee) {System.out.println(ee);}
             return;
         }
 
         try(PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("cowjump.out")))) {
-            // pw.println(res0 + 1);
-            System.out.println(res0);
+            pw.println(res0 + 1);
         } catch(Exception ee) {System.out.println(ee);}
 
     }
@@ -135,33 +117,35 @@ public class Main {
         A[i * 2 + 1] = tmp;
     }
 
+    // Between
+    public static boolean bt(long[] a, long[] b, long[] x) {
+        return (a[0] == b[0])
+            ? (a[1] <= x[1] && x[1] <= b[1] || b[1] <= x[1] && x[1] <= a[1])
+            : (a[0] <= x[0] && x[0] <= b[0] || b[0] <= x[0] && x[0] <= a[0]);
+    }
+
+    // Area
+    public static long aa(long[] a, long[] b, long[] c) {
+        return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0]);
+    }
+
     public static boolean check(int[] X, int[] Y, int a, int b) {
-        //TODO Complete rigorous line intersection check
 
-        long[] u = new long[]{X[b * 2] - X[a * 2], Y[b * 2] - Y[a * 2]};
-        long[] v = new long[]{X[b * 2 + 1] - X[a * 2], Y[b * 2 + 1] - Y[a * 2]};
-        long[] w = new long[]{X[a * 2 + 1] - X[a * 2], Y[a * 2 + 1] - Y[a * 2]};
+        long[] o = new long[]{X[a * 2], Y[a * 2]};
+        long[] u = new long[]{X[b * 2], Y[b * 2]};
+        long[] v = new long[]{X[b * 2 + 1], Y[b * 2 + 1]};
+        long[] w = new long[]{X[a * 2 + 1], Y[a * 2 + 1]};
 
-        // double alpha = 1. * (w[0] * v[1] - w[1] * v[0]) / (u[0] * v[1] - u[1] * v[0]);
-        // double beta = 1. * (-w[0] * u[1] + w[1] * u[0]) / (u[0] * v[1] - u[1] * v[0]);
-
-        // return alpha + beta >= 1 && alpha >= 0 && beta >= 0;
-
-        long alpha = w[0] * v[1] - w[1] * v[0];
-        long beta = -w[0] * u[1] + w[1] * u[0];
-        long gamma = u[0] * v[1] - u[1] * v[0];
+        long alpha = aa(o, u, w);
+        long beta = aa(o, w, v);
+        long gamma = aa(o, u, v);
 
         if(gamma > 0) return alpha + beta >= gamma && alpha >= 0 && beta >= 0;
         if(gamma < 0) return alpha + beta <= gamma && alpha <= 0 && beta <= 0;
 
-        if(
-                u[0] == 0 && u[1] == 0
-                v[0] == 0 && v[1] == 0
-
-
-
-                ) return true;
-
+        if(bt(u, v, o)) return true;
+        if(aa(u, w, v) != 0) return false;
+        return bt(o, w, u) || bt(o, w, v);
     }
 
 }
